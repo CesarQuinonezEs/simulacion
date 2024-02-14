46a9aad0ed829f752e42c446d3670885c1693658
CesarQuinonezEs <quinonez.cesar@uabc.edu.mx>
2024-02-13 23:46:29 -0800

    practica 3

--- practica3/practica3.py
+++ practica3/practica3.py
@@ -1,5 +1,20 @@
 from congrucialMixto import GPCM
 
-gpcm = GPCM(a=5,semilla=4,c=7,m=10)
-for i in range(8):
-    print(gpcm.generate())
+
+flag = True
+
+x0 = int(input("Ingrese la semilla "))
+a = int(input("Ingrese el multiplicador (a) "))
+c = int(input("Ingrese la constante aditiva (c) "))
+m = int(input("Ingrese el modulo "))
+gpcm = GPCM(a=a,semilla=x0,c=c,m=m)
+
+while flag:
+    n = int(input("Ingrese el periodo "))
+    print("Iteracion\t resultado\n")
+    for i in range(n):
+        
+        print(i,"\t\t\t",gpcm.generate(),"\n")
+    opc = input("Quiere volver a ingresar ? ")
+    if opc == "no":
+        flag = False
