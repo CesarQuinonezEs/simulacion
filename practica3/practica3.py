6f0e406c3fb04c70431ef8a889fc9caf7c74f11c
CesarQuinonezEs <quinonez.cesar@uabc.edu.mx>
2024-02-19 10:25:37 -0800

    push

--- practica3/practica3.py
+++ practica3/practica3.py
@@ -7,9 +7,10 @@ x0 = int(input("Ingrese la semilla "))
 a = int(input("Ingrese el multiplicador (a) "))
 c = int(input("Ingrese la constante aditiva (c) "))
 m = int(input("Ingrese el modulo "))
-gpcm = GPCM(a=a,semilla=x0,c=c,m=m)
+
 
 while flag:
+    gpcm = GPCM(a=a,semilla=x0,c=c,m=m)
     n = int(input("Ingrese el periodo "))
     print("Iteracion\t resultado\n")
     for i in range(n):
